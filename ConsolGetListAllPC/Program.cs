06e9c923f201ad42f53fd760d780ebb7656c9052
LeNGuyenWi <lenguyenwi@gmail.com>
2017-03-15 10:29:24 +0100

    first commit

---
+++ ConsolGetListAllPC/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsolGetListAllPC
+{
+    class Program
+    {
+        private static NetworkBrowser nwB;
+
+        static void Main(string[] args)
+        {
+            nwB = new NetworkBrowser();
+            ArrayList networkComputers = new ArrayList();
+            networkComputers = nwB.getNetworkComputers();
+            //nwB.getNetworkComputers();
+
+            foreach (string networkcomputer in networkComputers)
+            {
+                Console.WriteLine(networkcomputer);
+            }
+            Console.ReadLine();
+
+        }
+        public static void getMyComIpAdress()
+        {
+            string computerName = System.Net.Dns.GetHostName();
+            System.Net.IPHostEntry ipEntry = System.Net.Dns.GetHostEntry(computerName);
+            System.Net.IPAddress[] ipAddress = ipEntry.AddressList;
+
+            computerName = computerName + "|" + ipAddress[0].ToString();
+            Console.WriteLine(computerName);
+            Console.ReadLine();
+        }
+        public static void getListComputer()
+        {
+
+        }
+
+     
+    }
+}
